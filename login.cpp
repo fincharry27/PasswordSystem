22e07d1069fe1b3fa7b5aafeaf3ea00ca8756282
euantaylor77 <eqtaylor@dundee.ac.uk>
2020-10-27 14:03:19 +0000

    Update login.cpp
    message

--- login.cpp
+++ login.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include "authlib.h"
 
+
+
+getInput()
+{
+	string userName;
+	string userPassword;
+
+	cout << "Please enter your username. " << endl;
+	 cin >> userName;
+
+	cout << "Please enter the password. " << endl;
+	cin >> userPassword;
+
+	
+	//cout << userName << endl <<  userPassword  ; //just to see if it works
+
+	//code to call hashPassword function
+
+	//return userName + HashedPassword;
+}
+
 int main() {
   bool auth = true;
  
