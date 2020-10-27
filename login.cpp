bff9d6526729862403ec4acfd8cdbb36da934e3c
JamesRobb03 <jamrobb@dundee.ac.uk>
2020-10-27 13:28:48 +0000

    changed String -> std::string

--- login.cpp
+++ login.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "authlib.h"
+#include "login.h"
+
+using namespace std;
 
 int main() {
   bool auth = true;
  
   if (auth) authenticated("user");
   else rejected("user");
+}
+
+string hashfunction(string password){
+	
 }
